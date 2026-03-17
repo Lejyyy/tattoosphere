e84f078336359e21ed7719eae6fd1aad82d820d5
Jorys Djemili <jorys.dje69@gmail.com>
2026-03-17 17:25:55 +0100

    Fix geocoder: remove Redis cache to avoid load order issue

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -4,7 +4,6 @@ Geocoder.configure(
   distances: :spherical,
   timeout: 5,
   lookup: :nominatim,
-  cache: (Rails.env.production? ? Redis.new(url: ENV["REDIS_URL"]) : nil),
   http_headers: {
     "User-Agent" => "Tattoosphere/1.0 (contact@tattoosphere.com)",
     "Accept-Language" => "fr"
